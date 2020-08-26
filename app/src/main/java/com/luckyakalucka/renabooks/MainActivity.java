a77e17f1e474f41d6afab0f4e5834b3ef4a95fac
Luckyakalucka <gusar.max2014@gmail.com>
2020-08-26 16:54:17 +0300

    Initial commit

---
+++ app/src/main/java/com/luckyakalucka/renabooks/MainActivity.java
@@ -0,0 +1,14 @@
+package com.luckyakalucka.renabooks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
