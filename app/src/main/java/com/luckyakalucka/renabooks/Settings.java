d7f0a439eee2114c7cabba6612d16daedcdf285a
Luckyakalucka <gusar.max2014@gmail.com>
2020-08-28 22:54:11 +0300

    Naebka XD

--- app/src/main/java/com/luckyakalucka/renabooks/Settings.java
+++ app/src/main/java/com/luckyakalucka/renabooks/Settings.java
@@ -1,10 +1,12 @@
 package com.luckyakalucka.renabooks;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
 
@@ -12,10 +14,17 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings2);
+
+
     }
 
     public void onMyButtonClick(View view) {
-    Intent intent = new Intent(Settings.this, MainActivity.class);
-    startActivity(intent);
-}
-}
+        if (view.getId() == R.id.home) {
+            Intent intent = new Intent(Settings.this, MainActivity.class);
+            startActivity(intent);
+
+
+        }
+
+        }
+    }
