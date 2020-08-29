9668eed50d4443001bca8e11c700f03573052b01
Luckyakalucka <gusar.max2014@gmail.com>
2020-08-29 17:40:15 +0300

    Add Check Update ro Settings Activity

--- app/src/main/java/com/luckyakalucka/renabooks/Settings.java
+++ app/src/main/java/com/luckyakalucka/renabooks/Settings.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +20,29 @@ public class Settings extends AppCompatActivity {
 
     }
 
-    public void onMyButtonClick(View view) {
-        if (view.getId() == R.id.home) {
-            Intent intent = new Intent(Settings.this, MainActivity.class);
-            startActivity(intent);
-
+    Intent browserIntent;
+    public void onClick(View v) {
+        if (v.getId() == R.id.ivGit) {
+            browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Luckyakalucka/RenaBooks"));
+            startActivity(browserIntent);
 
         }
+
+        if (v.getId() == R.id.ivTg) {
+            browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/joinchat/AAAAAE6SacQIaPCblqpfYQ"));
+            startActivity(browserIntent);
 
         }
     }
+
+        public void onMyButtonClick (View view){
+            if (view.getId() == R.id.home) {
+                Intent intent = new Intent(Settings.this, MainActivity.class);
+                startActivity(intent);
+
+
+            }
+
+        }
+    }
+
